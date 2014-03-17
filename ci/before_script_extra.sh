2bc74bf7d0addc78dab0ab5f6c9f5ff8aa4b9855
sebv <seb.vincent@gmail.com>
2014-03-17 14:07:46 +0800

    travis fix

--- ci/before_script_extra.sh
+++ ci/before_script_extra.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set +e
-if [[ $CI_CONFIG == 'unit' ]]; then
-    npm install
-elif [[ $CI_CONFIG == 'build' ]]; then
+if [[ $CI_CONFIG == 'build' ]]; then
     ./ci/install-android.sh
     sudo grunt authorize
 fi
