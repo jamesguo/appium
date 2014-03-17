c48d8e5f10f4983aa367a38a68d7bdc28bfd2c05
sebv <seb.vincent@gmail.com>
2014-03-17 14:21:24 +0800

    travis fix

--- ci/script.sh
+++ ci/script.sh
@@ -3,5 +3,6 @@ set +e
 if [[ $CI_CONFIG == 'unit' ]]; then
     npm test
 elif [[ $CI_CONFIG == 'build' ]]; then
-    ./reset.sh --hardcore --real-safari --chromedriver-install-all --chromedriver-version 2.9 --verbose
+    ./reset.sh --hardcore --verbose
+    # ./reset.sh --hardcore --real-safari --chromedriver-install-all --chromedriver-version 2.9 --verbose
 fi
