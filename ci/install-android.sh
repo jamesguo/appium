f85381317b6fd5413237bb4ef7bf92223cc643f6
sebv <seb.vincent@gmail.com>
2014-03-17 12:24:17 +0800

    configuring travis

--- ci/install-android.sh
+++ ci/install-android.sh
@@ -8,7 +8,8 @@ popd
 export ANDROID_ROOT=$HOME/tools/android/adt-bundle-mac-x86_64-20131030
 export ANDROID_HOME=$ANDROID_ROOT/sdk
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-echo yes | android update sdk --filter platform-tools --no-ui --force 
-echo yes | android update sdk --filter android-18 --no-ui --force 
-echo yes | android update sdk --filter extra-android-support --no-ui --force
-echo yes | android update sdk --filter extra-android-m2repository --no-ui --force
+alias android_update=android update sdk --no-ui -a
+echo yes | android_update --filter platform-tools 
+echo yes | android_update --filter android-18 
+echo yes | android_update --filter extra-android-support
+echo yes | android_update --filter extra-android-m2repository
