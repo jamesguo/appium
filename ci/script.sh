eb729510a34105a5e55f4860e5d172c4badaf273
sebv <seb.vincent@gmail.com>
2014-03-18 10:52:44 +0800

    travis fix

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,8 @@ set +e
 if [[ $CI_CONFIG == 'unit' ]]; then
     npm test
 elif [[ $CI_CONFIG == 'build' ]]; then
+    echo "ANDROID_HOME --> $ANDROID_HOME"
+    echo "PATH --> $PATH"    
     ./reset.sh --hardcore --verbose
     # ./reset.sh --hardcore --real-safari --chromedriver-install-all --chromedriver-version 2.9 --verbose
 fi
